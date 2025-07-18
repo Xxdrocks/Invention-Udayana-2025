188b9cf4079a6e600c2623180c415250f47b9670
Xxdrocks <kerjaanpahri@gmail.com>
2025-07-18 15:44:41 -0700

    card and boxes update

--- javascript/main.js
+++ javascript/main.js
@@ -155,3 +155,18 @@ function autoScroll() {
 }
 
 autoScroll();
+
+const handleMouseMove = e => {
+  const {currentTarget: target } = e;
+
+  const rect = target.getBoundingClientRect(),
+   x = e.clientX - rect.left,
+   y = e.clientY - rect.top;
+
+   target.style.setProperty('--mouse-x', `${x}px`);
+   target.style.setProperty('--mouse-y', `${y}px`);
+}
+
+for(const box of document.querySelectorAll('.box')) {
+  box.onmousemove = e => handleMouseMove(e);
+}
